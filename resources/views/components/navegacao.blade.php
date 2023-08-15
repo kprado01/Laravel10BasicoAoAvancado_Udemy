c1bc258ecb17f86e9e17601d3b87b0c712f2aa4c
kaique.prado <kaique.prado@movida.com.br>
2023-08-15 20:43:33 -0300

    Adicionando index.blade.php e ajustando front

---
+++ resources/views/components/navegacao.blade.php
@@ -0,0 +1,38 @@
+<div class="sidebar border col-md-3 col-lg-2 bg-body-tertiary">
+    <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+        <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+                        <svg class="bi"><use xlink:href="#house-fill"/></svg>
+                        Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                        <svg class="bi"><use xlink:href="#graph-up"/></svg>
+                        Vendas
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                        <svg class="bi"><use xlink:href="#cart"/></svg>
+                        Produtos
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                        <svg class="bi"><use xlink:href="#people"/></svg>
+                        Clientes
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                        <svg class="bi"><use xlink:href="#door-closed"/></svg>
+                        Sair
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
